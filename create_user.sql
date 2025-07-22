987571c14a28cbc266061e44a9f7910bcd3ef22c
Hsherif2 <hsherif2@students.kennesaw.edu>
2025-07-21 21:42:44 -0400

    Update create_user.sql

--- create_user.sql
+++ create_user.sql
@@ -1,11 +1,13 @@
+
+-- USER TABLE
 CREATE TABLE user (
-  UID VARCHAR(50) PRIMARY KEY,
-  password VARCHAR(255) NOT NULL,
-  fName VARCHAR(50),
-  lName VARCHAR(50),
-  jobDescription VARCHAR(100),
-  viewPriveledgeYN CHAR(1),
-  insertPriveledgeYN CHAR(1),
-  updatePriveledgeYN CHAR(1),
-  deletePriveledgeYN CHAR(1)
+    UID VARCHAR(20) PRIMARY KEY NOT NULL,
+    password VARCHAR(20) NOT NULL,
+    fName VARCHAR(20) NOT NULL,
+    lName VARCHAR(20) NOT NULL,
+    jobDescription VARCHAR(50),
+    viewPriveledgeYN CHAR(1),
+    insertPriveledgeYN CHAR(1),
+    updatePriveledgeYN CHAR(1),
+    deletePriveledgeYN CHAR(1)
 );
