03aa7f6406f358971be5debc80513681d8fefdaf
Hsherif2 <hsherif2@students.kennesaw.edu>
2025-07-21 21:40:52 -0400

    Update Table_Descriptions.sql

--- Table_Descriptions.sql
+++ Table_Descriptions.sql
@@ -59,17 +59,3 @@ CREATE TABLE donations (
 
 
 
-
-
--- USER TABLE
-CREATE TABLE user (
-    UID VARCHAR(20) PRIMARY KEY NOT NULL,
-    password VARCHAR(20) NOT NULL,
-    fName VARCHAR(20) NOT NULL,
-    lName VARCHAR(20) NOT NULL,
-    jobDescription VARCHAR(50),
-    viewPriveledgeYN CHAR(1),
-    insertPriveledgeYN CHAR(1),
-    updatePriveledgeYN CHAR(1),
-    deletePriveledgeYN CHAR(1)
-);
